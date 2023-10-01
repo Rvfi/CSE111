a409d737f5d2b238fd82a93c5034a9f53c4c9364
Rafiul Islam <1726rafi@gmail.com>
2023-09-30 18:57:23 -0700

    Lab 4 hot fix

--- lab-4/test/3.sql
+++ lab-4/test/3.sql
@@ -3,6 +3,7 @@ SELECT c.c_name AS customer, SUM(o.o_totalprice) AS total_price
 FROM orders o
 JOIN customer c ON o.o_custkey = c.c_custkey
 JOIN nation n ON c.c_nationkey = n.n_nationkey
-WHERE n.n_name = 'ARGENTINA' AND EXTRACT(YEAR FROM o.o_orderdate) = 1996
+WHERE n.n_name = 'ARGENTINA' AND strftime('%Y', o.o_orderdate) = '1996'
 GROUP BY c.c_name
 ORDER BY c.c_name;
+
--- lab-4/test/9.sql
+++ lab-4/test/9.sql
@@ -5,7 +5,8 @@ JOIN lineitem l ON s.s_suppkey = l.l_suppkey
 JOIN orders o ON l.l_orderkey = o.o_orderkey
 JOIN nation n ON s.s_nationkey = n.n_nationkey
 JOIN region r ON n.n_regionkey = r.r_regionkey
-WHERE r.r_name = 'AFRICA' AND o.o_orderstatus = 'F' AND EXTRACT(YEAR FROM o.o_orderdate) = 1993
+WHERE r.r_name = 'AFRICA' AND o.o_orderstatus = 'F' AND strftime('%Y', o.o_orderdate) = '1993'
 GROUP BY n.n_name
 HAVING COUNT(DISTINCT o.o_orderkey) > 200
 ORDER BY n.n_name;
+
