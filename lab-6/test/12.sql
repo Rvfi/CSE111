ac080ecbc6e2926dfeba47f8456c2c3950013f3d
Rafiul Islam <1726rafi@gmail.com>
2023-10-15 20:07:05 -0700

    Lab 6 FINALIZED & reviewed

--- lab-6/test/12.sql
+++ lab-6/test/12.sql
@@ -1,9 +1,12 @@
 .headers on
-SELECT COUNT(DISTINCT p.p_partkey) AS part_cnt
-FROM part p
-JOIN partsupp ps ON p.p_partkey = ps.ps_partkey
-JOIN supplier s ON ps.ps_suppkey = s.s_suppkey
-JOIN nation n ON s.s_nationkey = n.n_nationkey
-WHERE n.n_name = 'UNITED STATES'
-GROUP BY p.p_partkey
-HAVING COUNT(DISTINCT s.s_suppkey) = 1;
+SELECT COUNT(*) AS part_cnt
+FROM (
+  SELECT p.p_partkey
+  FROM part p
+  JOIN partsupp ps ON p.p_partkey = ps.ps_partkey
+  JOIN supplier s ON ps.ps_suppkey = s.s_suppkey
+  JOIN nation n ON s.s_nationkey = n.n_nationkey
+  WHERE n.n_name = 'UNITED STATES'
+  GROUP BY p.p_partkey
+  HAVING COUNT(DISTINCT s.s_suppkey) = 1
+) AS UniqueParts;
--- lab-6/test/6.sql
+++ lab-6/test/6.sql
@@ -1,8 +1,11 @@
 .headers on
-SELECT COUNT(DISTINCT s.s_suppkey) AS suppliers_cnt
-FROM supplier s
-JOIN partsupp ps ON s.s_suppkey = ps.ps_suppkey
-JOIN nation n ON s.s_nationkey = n.n_nationkey
-WHERE n.n_name = 'PERU'
-GROUP BY s.s_suppkey
-HAVING COUNT(DISTINCT ps.ps_partkey) > 40;
+SELECT COUNT(*) AS suppliers_cnt
+FROM (
+  SELECT s.s_suppkey
+  FROM supplier s
+  JOIN partsupp ps ON s.s_suppkey = ps.ps_suppkey
+  JOIN nation n ON s.s_nationkey = n.n_nationkey
+  WHERE n.n_name = 'PERU'
+  GROUP BY s.s_suppkey
+  HAVING COUNT(DISTINCT ps.ps_partkey) > 40
+) AS UniqueSuppliers;
